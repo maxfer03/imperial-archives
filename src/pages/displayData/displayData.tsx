dd3ac6a4bb054b3d7fe3f9f2e61a3734f01a547a
Max Fer <sandsferdinand@gmail.com>
2021-07-29 22:07:27 -0300

    added basic searcher

--- src/pages/displayData/displayData.tsx
+++ src/pages/displayData/displayData.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom'
 import IPage from '../../interfaces/page'
 import logging from '../../config/logging'
@@ -10,17 +10,26 @@ const DisplayData: React.FunctionComponent<IPage & RouteComponentProps<any>> = p
 
     useEffect(() => {
         logging.info(`loading ${props.name}`)
-    },[props.name])
+    }, [props.name])
 
-    
-    
+
+
     return (
         <div>
             <div>
-                Here I display the Data
+                <form>
+                    <label>
+                        search: <input type='text' />
+                    </label>
+                </form>
+
+                <div>
+                    info displayed here
+                </div>
+                
             </div>
             <div>
-                <Link to = {homePath}>Go to HomePage</Link>
+                <Link to={homePath}>Go to HomePage</Link>
             </div>
         </div>
     )
