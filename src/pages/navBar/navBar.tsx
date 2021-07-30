5b61fd46c8d4819cc525bc4414f712d1bb9e176e
Max Fer <sandsferdinand@gmail.com>
2021-07-29 21:49:51 -0300

    basic navbar styling

--- src/pages/navBar/navBar.tsx
+++ src/pages/navBar/navBar.tsx
@@ -1,22 +1,25 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import IPage from '../../interfaces/page'
-import logging from '../../config/logging' 
+import logging from '../../config/logging'
 import { aboutPath, homePath, mainPath } from '../../interfaces/pagePaths'
+
+import style from './navBar.module.css'
 
 const NavBar: React.FunctionComponent<IPage> = props => {
 
-    
+
     useEffect(() => {
         logging.info(`loading ${props.name}`)
-    },[props.name])
-    
+    }, [props.name])
+
     return (
-        <div>
-            <div>this is the nav bar</div>
-            <Link to ={aboutPath}>About</Link>
-            <Link to ={mainPath}>Main</Link>
-            <Link to ={homePath}>Home</Link>
+        <div className={style.navbar}>
+            <div className={style.links}>
+                <Link to={aboutPath}>About</Link>
+                <Link to={mainPath}>Main</Link>
+                <Link to={homePath}>Home</Link>
+            </div>
         </div>
     )
 }
